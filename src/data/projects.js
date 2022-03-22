ce12adfc6ec165c7d6fe095768eb79768e1e6efa
xingcw <xingcw602@sina.com>
2022-03-22 01:12:05 +0100

    modify resume part.

--- src/data/projects.js
+++ src/data/projects.js
@@ -6,10 +6,7 @@ const data = [
     image: '/images/projects/nearestdollar.jpg',
     date: '2015-11-20',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+    '',
   },
   {
     title: 'Harvest',
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,6 +5,7 @@ import Main from '../layouts/Main';
 
 import Education from '../components/Resume/Education';
 import Experience from '../components/Resume/Experience';
+import Publications from '../components/Resume/Publications';
 import Skills from '../components/Resume/Skills';
 import Courses from '../components/Resume/Courses';
 import References from '../components/Resume/References';
@@ -12,10 +13,12 @@ import References from '../components/Resume/References';
 import courses from '../data/resume/courses';
 import degrees from '../data/resume/degrees';
 import positions from '../data/resume/positions';
+import publications from '../data/resume/publications';
 import { skills, categories } from '../data/resume/skills';
 
 const sections = [
   'Education',
+  'Publications',
   'Experience',
   'Skills',
   'Courses',
@@ -25,7 +28,7 @@ const sections = [
 const Resume = () => (
   <Main
     title="Resume"
-    description="Chunwei Xing's Resume. Arthena, Matroid, YC, Skeptical Investments, Stanford ICME, Planet Labs, and Facebook."
+    description="Chunwei Xing's Resume."
   >
     <article className="post" id="resume">
       <header>
@@ -41,6 +44,7 @@ const Resume = () => (
         </div>
       </header>
       <Education data={degrees} />
+      <Publications data={publications} />
       <Experience data={positions} />
       <Skills skills={skills} categories={categories} />
       <Courses data={courses} />
