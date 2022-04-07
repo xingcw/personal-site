5745d0aaadab19acd8d52dfce3fd009d1c4c114c
xingcw <xingcw602@sina.com>
2022-04-07 17:52:39 +0200

    minor changes

--- src/data/projects.js
+++ src/data/projects.js
@@ -10,12 +10,12 @@ const data = [
     author: 'Chunwei Xing',
     desc:
     'The motion planning problem for wheeled-legged robots is complicated on challenging terrains. '
-    + 'In our previous work, a combined sampling and optimization based method is proposed to solve the problem.'
-    + 'We expect to further improve the efficiency of the sampling-based planner by substituting the heuristic base pose selector'
+    + 'In our previous work, a combined sampling and optimization based method is proposed to solve the problem. '
+    + 'We expect to further improve the efficiency of the sampling-based planner by substituting the heuristic base pose selector '
     + 'with a learning-based method. We formulate a Reinforcement Learning (RL) problem for the base pose selector and '
-    + 'optimize the reward functions to excel the potential of learning policy. In this project, we further optimize the RL policy'
-    + 'by experimenting with different training methods. We formulate evaluation metrics for the RL testing and benchmark the performance of our learned policy'
-    + 'based on it. Besides, we benchmark the performance of brute force search method for comparison. In the end, we finish the preparation steps'
+    + 'optimize the reward functions to excel the potential of learning policy. In this project, we further optimize the RL policy '
+    + 'by experimenting with different training methods. We formulate evaluation metrics for the RL testing and benchmark the performance of our learned policy '
+    + 'based on it. Besides, we benchmark the performance of brute force search method for comparison. In the end, we finish the preparation steps '
     + 'to integrate our learned policy into the global planner.',
   },
   {
@@ -27,14 +27,14 @@ const data = [
     date: 'Feb. 2021 - Jun. 2021',
     author: 'Chunwei Xing | Xinyu Sun',
     desc:
-      'Current global localization descriptors in Simultaneous Localization and Mapping (SLAM) often fail'
-      + 'under vast viewpoint or appearance changes. Adding topological information of semantic objects into the descriptors'
-      + 'ameliorates the problem. However, hand-crafted topological descriptors extract limited information'
-      + 'and they are not robust to environmental noise, drastic perspective changes, or object occlusion or misdetections.'
-      + 'To solve this problem, we formulate a learning-based approach by constructing constellations from semantically meaningful objects'
-      + 'and use Deep Graph Convolution Networks to map the constellation representation to a descriptor. We demonstrate the effectiveness'
-      + 'of our Deep Learned Constellation Descriptor (Descriptellation) on the Paris-Rue-Lille and IQmulus datasets. Although Descriptellation'
-      + 'is trained on randomly generated simulation datasets, it shows good generalization abilities on real-world datasets.'
+      'Current global localization descriptors in Simultaneous Localization and Mapping (SLAM) often fail '
+      + 'under vast viewpoint or appearance changes. Adding topological information of semantic objects into the descriptors '
+      + 'ameliorates the problem. However, hand-crafted topological descriptors extract limited information '
+      + 'and they are not robust to environmental noise, drastic perspective changes, or object occlusion or misdetections. '
+      + 'To solve this problem, we formulate a learning-based approach by constructing constellations from semantically meaningful objects '
+      + 'and use Deep Graph Convolution Networks to map the constellation representation to a descriptor. We demonstrate the effectiveness '
+      + 'of our Deep Learned Constellation Descriptor (Descriptellation) on the Paris-Rue-Lille and IQmulus datasets. Although Descriptellation '
+      + 'is trained on randomly generated simulation datasets, it shows good generalization abilities on real-world datasets. '
       + 'Descriptellation outperforms the PointNet and handcrafted constellation descriptors for global localization, and shows robustness against different types of noise.',
   },
   {
@@ -51,16 +51,6 @@ const data = [
       + 'We introduced keyframe machnism into our pipeline and used KLT to track frame-frame correspondences and SURF features for keyframe-keyframe correspondences. '
       + 'We also introduced sliding window Bundle Adjustment method for optimization. ',
   },
-  // {
-  //   title: 'Cat Detector',
-  //   subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-  //   image: '/images/projects/catdetector.jpg',
-  //   date: '2015-05-15',
-  //   desc:
-  //     'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-  //     + 'Over 60,000 cats were classified before server bills made the project too expensive '
-  //     + 'to continue hosting.',
-  // },
 ];
 
 export default data;
